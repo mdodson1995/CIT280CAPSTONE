5989fe9e38f2b7209957e33c76720ea7618f1783
mdodson1995 <m.dodson@hotmail.com>
2017-02-15 09:06:49 -0500

    2_15 Beginning Structure

---
+++ CustomerRelationsManager/CustomerRelationsManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomerRelationsManager.MvcApplication" Language="C#" %>
---
+++ CustomerRelationsManager/CustomerRelationsManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerRelationsManager.Startup))]
+namespace CustomerRelationsManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
